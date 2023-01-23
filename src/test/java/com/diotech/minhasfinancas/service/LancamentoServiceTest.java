a0e1ada0e637095469d8c694c02b4fd4ae3d3257
dione-is <dione.bombeta@outlook.com>
2023-01-23 14:19:43 -0300

    implementado Testes na classe LancementoSercevice Test, salvar, atualizar, validar, deletar

---
+++ src/test/java/com/diotech/minhasfinancas/service/LancamentoServiceTest.java
@@ -0,0 +1,65 @@
+package com.diotech.minhasfinancas.service;
+
+import com.diotech.minhasfinancas.entity.Lancamento;
+import com.diotech.minhasfinancas.entity.Usuario;
+import com.diotech.minhasfinancas.enums.TipoLancamento;
+import com.diotech.minhasfinancas.service.impl.UsuarioServiceImpl;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.math.BigDecimal;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class LancamentoServiceTest {
+
+    @Autowired
+    LancamentoService service;
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Test
+    @Order(2)
+    public void validar() {
+        Assertions.assertDoesNotThrow(() -> service.validar(createLancamento()));
+    }
+
+    @Test
+    @Order(1)
+    public void salvar() {
+        usuarioService.salvarUsuario(new Usuario(1L, "dione", "dione@email", "123mudar"));
+        org.assertj.core.api.Assertions.assertThat(service.salvar(createLancamento()).getId()).isNotNull();
+    }
+
+    @Test
+    @Order(3)
+    public void atualizar(){
+        Lancamento oldLancamento = service.obterLancamentoPorId(1L).orElse(null);
+        oldLancamento.setValor(new BigDecimal(1580.80));
+        org.assertj.core.api.Assertions.assertThat(service.atualizar(oldLancamento)).isNotNull();
+    }
+
+    @Test
+    @Order(4)
+    public void deletar(){
+        service.deletar(1L);
+        Assertions.assertNull(service.obterLancamentoPorId(1L).orElse(null));
+    }
+
+    public Lancamento createLancamento() {
+        return Lancamento.builder()
+                .ano(2022)
+                .mes(1)
+                .descricao("teste")
+                .usuario(new Usuario(1L, "dione", "dione@email", "123mudar"))
+                .tipo(TipoLancamento.RECEITA)
+                .valor(new BigDecimal(580.80)).build();
+    }
+}
